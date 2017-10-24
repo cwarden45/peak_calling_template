6d4a0f84e8ba740311d6046e0daa9af77dee8e2b
Charles Warden <cwarden45@gmail.com>
2017-10-24 13:33:19 -0700

    Add files via upload

---
+++ extract_peak_sequences.pl
@@ -0,0 +1,69 @@
+use warnings;
+use strict;
+use Bio::SeqIO;
+
+my $bed_file = "../Result/i-cisTarget_motif/input_files/[compID]_[direction].bed";
+my $ref = "/path/to/ref.fa";
+my $peak_seq = "[compID]_[direction].fa";
+
+#if peaks must be same size, select set distance from middle of peak (unless set to "-1", then keep full sequence)
+my $set_length = -1;
+
+my $seqio_obj = Bio::SeqIO->new(-file => $ref, 
+								-format => "fasta" );
+my %seq_hash;
+
+while (my $seq_obj = $seqio_obj->next_seq){
+	my $chr_name = $seq_obj->id;
+	my $chr_seq = $seq_obj->seq;
+	$seq_hash{$chr_name}=$chr_seq;
+}#end while (my $seq_obj = $seqio_obj->next_seq)
+
+open(OUT, "> $peak_seq") || die("Could not open  $peak_seq!");
+								
+open(IN, $bed_file)|| die("Cannot open $bed_file!\n");
+
+my $line_count = 0;
+
+while(<IN>){
+	my $line = $_;
+	chomp $line;
+	$line =~ s/\r//g;
+	$line =~ s/\n//g;
+	
+	$line_count++;
+	
+	if($line_count > 1){
+		my @line_info = split("\t",$line);
+		
+		my $peak_chr = $line_info[0];
+		my $peak_start = $line_info[1];
+		my $peak_stop = $line_info[2];
+		my $peak_name = "$peak_chr:$peak_start-$peak_stop";
+		
+		my $chr_seq = $seq_hash{$peak_chr};
+		
+		my $peak_seq = "";
+		
+		if($set_length == -1){
+			$peak_seq = uc(substr($chr_seq, $peak_start-1,$peak_stop-$peak_start+1));
+		}else{
+			#print "Centered, Fixed-Length Sequence\n";
+			my $peak_center = int(($peak_stop+$peak_start)/2);
+			my $flank_distance = int($set_length/2);
+			$peak_start=$peak_center-$flank_distance;
+			$peak_stop=$peak_center+$flank_distance;
+			$peak_seq = uc(substr($chr_seq, $peak_start-1,$peak_stop-$peak_start+1));
+			#print "$peak_center:$flank_distance:$peak_start-$peak_stop\n";
+			#print ">$peak_name\n$peak_seq\n";
+		}#end else
+		
+		print OUT ">$peak_name\n$peak_seq\n";
+	}#end if($line_count > 1)
+}#end while(<IN>)
+
+close(IN);
+
+close(OUT);						
+
+exit;
---
+++ run_MEME-chip.pl
@@ -0,0 +1,15 @@
+use warnings;
+use strict;
+
+my $peakFA = "[compID]_[direction].fa";
+my $dbRoot = "/path/to/meme_4.12.0/motif_databases";
+my @motifDBs = ("$dbRoot/HUMAN/HOCOMOCOv10_HUMAN_mono_meme_format.meme",
+				"$dbRoot/EUKARYOTE/jolma2013.meme",
+				"$dbRoot/EUKARYOTE/macisaac_theme.v1.meme",
+				"$dbRoot/JASPAR/JASPAR_CORE_2016_vertebrates.meme");
+my $outputfolder = "[compID]_[direction]";
+
+my $command = "meme-chip -o $outputfolder -db ".join(" -db ",@motifDBs)." $peakFA";
+system($command);
+
+exit;
